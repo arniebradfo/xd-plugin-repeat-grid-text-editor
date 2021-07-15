dbbe1969cb0021033b68b2257df831566eac6a34
James Bradford <j.bradford@pnnl.gov>
2021-07-14 18:53:23 -0700

    this won't work. Can't change the editContext

--- main.js
+++ main.js
@@ -2,59 +2,67 @@
 const { RepeatGrid } = require("scenegraph");
 
 function tabToNext(selection) {
+    console.log(selection);
 
-    const selectedRepeatGridItem = selection.items[0] // check to see if text?
+    const selectedRepeatGridItem = selection.items[0]
+    // check to see if text?
+    // TODO: multiple selection.items?
 
-    // bubble up the SceneGraph, recording the guid of the parentNode and the index of the currentNode
-    // stop at the first Repeat Grid
-    const pathFromSelected = findPathToAncestorOfType(selectedRepeatGridItem, RepeatGrid)
+    // bubble up the SceneGraph, recording the guid of the parentNode and the index of the currentNode, stopping at the firstRepeatGrid
+    const [repeatGrid, pathFromSelected] = findPathToAncestorOfType(selectedRepeatGridItem, RepeatGrid)
     console.log(pathFromSelected);
 
-    // find next instance
-    
-    // increment the root.childIndex to select the next element
-    const repeatGridPathRoot = pathFromSelected[0]
-    let repeatGridNextIndex = repeatGridPathRoot.childIndex + 1 // for top2bottom increment by rowCount+1
-    if (repeatGridNextIndex === repeatGridPathRoot.node.children.length)
-        repeatGridNextIndex = 0
-    pathFromSelected[0].childIndex = repeatGridNextIndex
+    if (repeatGrid == null)
+        return false
 
-    const nextItemToSelect = findDecedentFromPath(pathFromSelected, repeatGridPathRoot.node)
+    // increment the root.childIndex to select the next element
+    let selectedCellIndex = pathFromSelected[0]
+    
+    // TODO: for top2bottom traversal, increment by rowCount+1
+    selectedCellIndex = selectedCellIndex + 1
+    
+    // if were' at the last item restart from the first
+    if (selectedCellIndex === repeatGrid.children.length)
+        selectedCellIndex = 0
+    
+    pathFromSelected[0] = selectedCellIndex
+
+    // traverse the Repeat grid to find the next item
+    const nextItemToSelect = findDecedentFromPath(pathFromSelected, repeatGrid)
+    // TODO: if null check ?
     console.log(nextItemToSelect.text);
 
-    // find the index of the RepeatGrid item that is selected.
-    // find the index/id of the Group item that is selected.
-    // move to the next instance of that item, or the first if last
+    // TODO: Update Selection ? // https://adobexdplatform.com/plugin-docs/reference/selection.html
+    // This is not possible due to plugin actions being limited to the editContext 
+    // https://adobexdplatform.com/plugin-docs/reference/core/edit-context.html
 
 }
 
 function findDecedentFromPath(pathFromSelected, rootNode) {
     let currentNode = rootNode
-    pathFromSelected.forEach(pathNode => {
-        currentNode = currentNode.children.at(pathNode.childIndex)
+    pathFromSelected.forEach(childIndex => {
+        if (currentNode == null)
+            return false
+        currentNode = currentNode.children.at(childIndex)
     })
     return currentNode
 }
 
-function findPathToAncestorOfType(node, type) {
+function findPathToAncestorOfType(node, Type) {
     let path = []
     let activeNode = node
-    while ((activeNode != null) && !(activeNode instanceof type)) {
-        path.push({
-            node: activeNode.parent,
-            childIndex: indexOfChildInParent(activeNode)
-        })
+    while (!(activeNode instanceof Type)) {
+        if (activeNode == null)
+            return [false, []]
+        path.push(indexOfChildInParent(activeNode))
         activeNode = activeNode.parent
     }
-    if (activeNode.parent == null)
-        return false
-    else
-        return path.reverse()
+    return [activeNode, path.reverse()] // activeNode is of Type
 }
 
 function indexOfChildInParent(childNode) {
     let index = 0
-    // console.log(childNode.parent.children);
+    // console.log(childNode.parent.children)
     childNode.parent.children.some((child) => {
         if (child.guid === childNode.guid) {
             return true
