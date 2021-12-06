b5d1eb1228d9030fffe2beb6110b456f99d791b7
James Bradford <j.bradford@pnnl.gov>
2021-12-05 17:18:31 -0800

    getting textDataseries from RepeatGrid

--- src/createTextDataSeries.ts
+++ src/createTextDataSeries.ts
@@ -1,7 +1,7 @@
-import { CommandHandler, RepeatGrid, SceneNode, Text } from "scenegraph";
+import { CommandHandler, GraphicNode, RepeatGrid, SceneNode, Text } from "scenegraph";
 
 export const createTextDataSeries: CommandHandler = function (selection, root) {
-    console.log({selection, root});
+    console.log({ selection, root });
 
     const selectedRepeatGridItem = selection.items[0]
     // check to see if text?
@@ -10,26 +10,44 @@ export const createTextDataSeries: CommandHandler = function (selection, root) {
     // bubble up the SceneGraph, recording the guid of the parentNode and the index of the currentNode, stopping at the firstRepeatGrid
     const findPathToRepeatGridAncestor = findPathToAncestorOfType<RepeatGrid>(selectedRepeatGridItem, RepeatGrid)
     if (!findPathToRepeatGridAncestor) return false // not a child of RepeatGrid
-    const [repeatGrid, pathFromSelected] = findPathToRepeatGridAncestor
+    const { node: repeatGrid, indexPath: pathFromSelected } = findPathToRepeatGridAncestor
+    console.log(repeatGrid)
 
-    console.log(repeatGrid);
+    // traverse repeatGrid first child and set of Text Nodes 
+    const exampleRepeatCell = repeatGrid.children.at(0)
+    const leaves = getSceneNodeLeaves(exampleRepeatCell)
 
-    // increment the root.childIndex to select the next element
-    let selectedCellIndex = pathFromSelected[0]
+    // transform leaves into textDataSeries
+    const repeatGridTextDataSeries: { name: string, textDataSeries: string[]}[] = []
+    leaves.forEach(leaf => {
+        const textDataSeries = []
+        repeatGrid.children.forEach(repeatCell => {
+            textDataSeries.push((findDecedentFromPath(repeatCell, leaf.indexPath) as Text).text)
+        })
+        const name = (leaf.node as Text).name // leaf.node.hasDefaultName may be useful?
+        repeatGridTextDataSeries.push({
+            name,
+            textDataSeries
+        })   
+    });
+    console.log(repeatGridTextDataSeries);
+    
+    // // increment the root.childIndex to select the next element
+    // let selectedCellIndex = pathFromSelected[0]
 
-    // TODO: for top2bottom traversal, increment by rowCount+1
-    selectedCellIndex = selectedCellIndex + 1
+    // // TODO: for top2bottom traversal, increment by rowCount+1
+    // selectedCellIndex = selectedCellIndex + 1
 
-    // if were' at the last item restart from the first
-    if (selectedCellIndex === repeatGrid.children.length)
-        selectedCellIndex = 0
+    // // if were' at the last item restart from the first
+    // if (selectedCellIndex === repeatGrid.children.length)
+    //     selectedCellIndex = 0
 
-    pathFromSelected[0] = selectedCellIndex
+    // pathFromSelected[0] = selectedCellIndex
 
-    // traverse the Repeat grid to find the next item
-    const nextItemToSelect = findDecedentFromPath(pathFromSelected, repeatGrid) as Text
-    // TODO: if null check ?
-    console.log(nextItemToSelect.text);
+    // // traverse the Repeat grid to find the next item
+    // const nextItemToSelect = findDecedentFromPath(repeatGrid, pathFromSelected) as Text
+    // // TODO: if null check ?
+    // console.log(nextItemToSelect.text);
 
     // TODO: Update Selection ? // https://adobexdplatform.com/plugin-docs/reference/selection.html
     // This is not possible due to plugin actions being limited to the editContext 
@@ -37,28 +55,54 @@ export const createTextDataSeries: CommandHandler = function (selection, root) {
 
 }
 
-function findDecedentFromPath(pathFromSelected:number[], rootNode:SceneNode): SceneNode {
-    let currentNode = rootNode
-    pathFromSelected.forEach(childIndex => {
+type NodeAndPath<T extends SceneNode = SceneNode> = {
+    node: T,
+    indexPath: number[]
+}
+
+function getSceneNodeLeaves(
+    node: SceneNode,
+    indexPath: number[] = []
+): NodeAndPath<GraphicNode>[] {
+    if (node.children.length === 0) {
+        const graphicNode = node as GraphicNode
+        return [{ node: graphicNode, indexPath }]
+    }
+    const leaves = []
+    node.children.forEachRight((node, ii) => {
+        leaves.push(getSceneNodeLeaves(node, [...indexPath, ii]))
+    })
+    return leaves.flat()
+}
+
+function findDecedentFromPath(
+    node: SceneNode,
+    indexPath: number[],
+): SceneNode {
+    let currentNode = node
+    indexPath.forEach(childIndex => {
         if (currentNode == null)
-            return false 
+            return false
         currentNode = currentNode.children.at(childIndex)
     })
     return currentNode
 }
 
-function findPathToAncestorOfType<T extends SceneNode>(
+function findPathToAncestorOfType<T extends SceneNode = SceneNode>(
     node: SceneNode,
     Type: (typeof SceneNode)
-): [T, number[]] | false {
-    let path:number[] = []
+): NodeAndPath<T> | false {
+    let path: number[] = []
     let activeNode: SceneNode = node
     while (!(activeNode instanceof Type)) {
         if (activeNode == null) return false
         path.push(indexOfChildInParent(activeNode))
         activeNode = (activeNode as SceneNode).parent
     }
-    return [activeNode as T, path.reverse()] // activeNode is of Type
+    return {
+        node: activeNode as T,
+        indexPath: path.reverse()
+    }
 }
 
 function indexOfChildInParent(
