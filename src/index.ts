78f095ae0700a54e914ed4ec06683f1deadbfcb9
James Bradford <j.bradford@pnnl.gov>
2021-12-12 16:41:29 -0800

    tsc failure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { createTextDataSeries } from "./createTextDataSeries";
-import { logSelection } from "./logSelection";
+import './react-shim'
+import './node_modules/react/index.js'
+import './node_modules/react-dom/index.js'
+// import { logSelection } from "./logSelection";
 import { textDataSeriesEditor } from "./textDataSeriesEditor";
-import { CommandHandler } from "scenegraph";
-type CommandId = string
+// import { CommandHandler } from "scenegraph";
+// type CommandId = string
 
-export const commands: {
-    [key: CommandId]: CommandHandler
-} = {
-    createTextDataSeries,
-    editRepeatContent: logSelection,
-}
+// export const commands: {
+//     [key: CommandId]: CommandHandler
+// } = {
+//     createTextDataSeries,
+//     editRepeatContent: logSelection,
+// }
 
 export const panels = {
     textDataSeriesEditor
---
+++ src/textDataSeriesEditor.tsx
@@ -0,0 +1,37 @@
+// import { editDocument } from "application";
+// import { RepeatGrid } from "scenegraph";
+// import { createTextDataSeries, RepeatGridTextDataSeries } from "./createTextDataSeries";
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// globals?
+let panel: HTMLDivElement;
+// let repeatGridTextObjects: RepeatGridTextDataSeries | undefined;
+
+// https://www.adobe.io/xd/uxp/develop/tutorials/quick-start-panel/#create-the-ui
+function create() {
+    
+    panel = document.createElement("div"); // [9]
+    // ReactDOM.render(<App />, panel);
+
+    console.log(panel);
+    
+    return panel;
+}
+
+// const App = (props) => (<span>Working</span>);
+
+// https://www.adobe.io/xd/uxp/develop/tutorials/quick-start-panel/#show-the-ui
+function show(event) {
+    if (!panel) event.node.appendChild(create()); // [2]
+}
+
+// https://www.adobe.io/xd/uxp/develop/tutorials/quick-start-panel/#update-your-ui
+function update(selection, root) {
+
+}
+
+export const textDataSeriesEditor = {
+    show,
+    update
+}
